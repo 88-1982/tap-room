d9142633a215c62e6f0f8cc75b25e73e5334c408
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2022-01-15 22:44:25 -0800

    add conditional to code to prompt pint sell

---
+++ src/components/KegDetail.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetail(props){
+  const { keg, onClickingDelete, onClickingEdit, onClickingSell } = props;
+  function decrementPintsByOne() {
+    if (keg.pints > 0) {
+      keg.pints = keg.pints-1;
+      onClickingSell(keg);
+    } else {
+      alert("Can't have less than 0!");
+    }
+  }
+  return (
+    <>
+      <h1>Keg Detail</h1>
+      <h3>Name: {keg.names} - Brand: {keg.brand}</h3>
+      <p>Price: ${keg.price}/pint</p>
+      <p>Alcohol Content: <strong>{keg.alcohol}%</strong></p>
+      <p><em>Availability: {keg.pints}pts</em></p>
+      <button onClick={ onClickingEdit }>Edit Keg</button>
+      <button onClick={ decrementPintsByOne }>Sell one pint</button>
+      <button onClick={()=> onClickingDelete(keg.id)}>Remove Keg</button>
+      <hr />
+    </>
+  );
+}
+
+KegDetail.propTypes = {
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default KegDetail;

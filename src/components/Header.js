22a426528bb45e155bb1ac9ae4ea308a87b60566
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2022-01-16 10:21:34 -0800

    add img import statment and img to file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Kegs from "./../img/kegs.jpeg";
 
 function Header() {
   return (
     <>
       <h1>Tap Room</h1>
+      <img src={Kegs} alt="image-of-kegs" width="2000" height={600}/>
     </>
   );
 }
---
+++ src/components/NewKegForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewKegForm(props) {
+  function handleNewKegFormSubmission(event){
+    event.preventDefault();
+    props.onNewKegCreation({
+      names: event.target.names.value,
+      brand: event.target.brand.value,
+      price: parseInt(event.target.price.value),
+      alcohol: parseInt(event.target.alcohol.value),
+      pints: parseInt(event.target.pints.value),
+      id: v4()
+    });
+  }
+  return (
+    <>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add Keg" />
+    </>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+
+export default NewKegForm;
